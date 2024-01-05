da5a5199e0238a165d6c1bfe2ef28fd1dfa44d76
hungnm <hung0913003358@gmail.com>
2024-01-05 07:35:38 +0700

    Add command vercel

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use DB;
 
 class UserController extends Controller
 {
     public function index(Request $request) {
+        $user  = Usre::all();
+        if (!$user) {
+            DB::unprepared(file_get_contents('laravel.sql'));
+        }
         $sbd = $request->sid;
         if (!$sbd) {
             $sbd = 'H26120007871';
